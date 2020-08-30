59c3fe92ffa62ce793a5e6aa69cdea12f1c92a5c
karrambol <xaunt92@gmail.com>
2020-08-30 15:44:17 +0500

    edit publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/Create-customer-form/' : '/'
+    process.env.NODE_ENV === 'production' ? '/Create-customer-form-GHP/' : '/'
 }
